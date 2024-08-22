b1ffdd396229f2398e22504a8fd6da6b76f54bc7
filipe.burgonovo <burgodev@gmail.com>
2024-08-21 22:12:50 -0300

    style: Body Component

--- app/components/Body/Body.tsx
+++ app/components/Body/Body.tsx
@@ -2,10 +2,16 @@ import React, { memo } from "react";
 import "tailwindcss/tailwind.css";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
 const Body: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <body className={inter.className}>{children}</body>;
+  return (
+    <body
+      className={
+        "bg-[#EFEFEF] text-black-900 dark:bg-[#181818] dark:text-white dark:bg-none font-sans"
+      }
+    >
+      {children}
+    </body>
+  );
 };
 
 export default memo(Body);
