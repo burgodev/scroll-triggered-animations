d02b0bb0bfd770300ad668939169c7ea2804b196
filipe.burgonovo <burgodev@gmail.com>
2024-08-21 22:13:30 -0300

    style: Footer Styles

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -6,12 +6,11 @@ const Footer: React.FC = () => {
     /* todo: color theme bg-#101010*/
   }
   return (
-    <footer className="w-[100%] h-[50vh] grid grid-cols-12 bg-[#101010] py-10 text-white text-[24px]">
-      <div className="flex flex-col col-span-6 col-start-7 justify-end max-w-[820px] gap-6">
-        <span className="text-[80px] underline">artem@ottofeller.com</span>
-        {/* <div className="h-[4px] w-[100%] "></div> */}
+    <footer className="w-[100%] h-[820px] grid grid-cols-12 py-[52px] text-[24px] dark:bg-white dark:text-[#101010] bg-[#101010] text-white dark:bg-none font-sans">
+      <div className="flex flex-col col-span-6 col-start-7 justify-end max-w-[820px] gap-6 px-3">
+        <p className="text-[80px] underline">artem@ottofeller.com</p>
 
-        <div className="flex gap-6 underline mt-2">
+        <div className="flex gap-6 underline mt-[52px]">
           {/* TODO: a can be component */}
           <a className="hover:cursor-pointer">Blog</a>
           <a className="hover:cursor-pointer">Policies</a>
