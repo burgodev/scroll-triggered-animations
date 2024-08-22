a8b0aba95288a53725c9d0cdb037d19f4e280cb1
filipe.burgonovo <burgodev@gmail.com>
2024-08-22 17:43:44 -0300

    feat: Header Z-Index

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { HomeButton } from "../HomeButton";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex justify-between fixed top-0 w-[100%] py-[38px] px-[52px]">
+    <header className="flex justify-between fixed top-0 w-[100%] py-[38px] px-[52px] z-10">
       <HomeButton />
       <ThemeToggle />
     </header>
