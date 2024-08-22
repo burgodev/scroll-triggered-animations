40066ac6686b1e5320a01fd5f320702354ee7915
filipe.burgonovo <burgodev@gmail.com>
2024-08-22 18:23:42 -0300

    chore: Add Header & Footer on Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import { Body } from "./components/Body";
+import { Header } from "./components/Header";
+import { Footer } from "./components";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Body>{children}</Body>
+      <Body>
+        <Header />
+        <main className="flex min-h-screen flex-col items-center justify-between p-2 sm:p-6 md:p-8 lg:p-10 xl:p-12 pb-[320px]">
+          {children}
+        </main>
+        <Footer />
+      </Body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 import "tailwindcss/tailwind.css";
-import { Welcome, Portfolio, Footer } from "./components";
-import { Header } from "./components/Header";
+import { Welcome, Portfolio } from "./components";
 
 const Home = () => {
   return (
-    <div>
-      <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-2 sm:p-6 md:p-8 lg:p-10 xl:p-12 pb-[320px]">
-        <Welcome />
-        <Portfolio />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Welcome />
+      <Portfolio />
+    </>
   );
 };
 
