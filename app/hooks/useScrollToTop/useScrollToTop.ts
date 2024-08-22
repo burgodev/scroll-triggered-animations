307b42421c004a99a9f06d4367d4ba151bc3609f
filipe.burgonovo <burgodev@gmail.com>
2024-08-22 17:43:02 -0300

    chore: useScrollToTop Hook

---
+++ app/hooks/useScrollToTop/useScrollToTop.ts
@@ -0,0 +1,9 @@
+const useScrollToTop = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return scrollToTop;
+};
+
+export default useScrollToTop;
